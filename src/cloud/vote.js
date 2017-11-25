9eb9eebf9d2e10264beb7ee73342d4fd78951c68
yang24201 <yang24201@126.com>
2017-11-26 00:06:14 +0800

    add save vote info timer

--- src/cloud/vote.js
+++ src/cloud/vote.js
@@ -4,6 +4,14 @@
 import AV from '../utils/av-weapp-min'
 
 export default class vote {
+  static VOTE_STATUS = {
+    EDITING: 1,     // 正在编辑
+    PAYING: 2,      // 待支付
+    WAITING: 3,     // 未开始
+    STARTING: 4,    // 正在进行
+    DONE: 5,        // 已结束
+  }
+  
   static async fetchGifts() {
     try {
       let awards = await AV.Cloud.run('voteFetchGifts')
@@ -15,11 +23,12 @@ export default class vote {
   }
   
   static async createVote(payload) {
-    try {
-      let vote = await AV.Cloud.run('voteCreateVote', payload)
-      return vote
-    } catch (e) {
-      throw e
-    }
+    let vote = await AV.Cloud.run('voteCreateVote', payload)
+    return vote
+  }
+  
+  static async createOrUpdateVote(payload) {
+    let vote = await AV.Cloud.run('voteCreateOrUpdateVote', payload)
+    return vote
   }
 }
